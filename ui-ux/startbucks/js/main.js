edd7127c954ab6bca05555778c6975560c199769
ooyuo <doyounjeong0@gmail.com>
2023-03-10 05:34:13 -0500

    build login page

--- ui-ux/startbucks/js/main.js
+++ ui-ux/startbucks/js/main.js
@@ -1,22 +1,3 @@
-console.log("hi");
-// #1. search bar
-const searchEl = document.querySelector(".search");
-const searchInputEl = searchEl.querySelector("input");
-
-searchEl.addEventListener("click", function () {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener("focus", function () {
-  searchEl.classList.add("focused");
-  searchInputEl.setAttribute("placeholder", "통합검색");
-});
-
-searchInputEl.addEventListener("blur", function () {
-  searchEl.classList.remove("focused");
-  searchInputEl.setAttribute("placeholder", "");
-});
-
 // #2. badge
 const badgeEl = document.querySelector("header .badges");
 const toTopEl = document.querySelector("#to-top");
@@ -149,7 +130,3 @@ spyEls.forEach(function (spyEl) {
     .setClassToggle(spyEl, "show")
     .addTo(new ScrollMagic.Controller());
 });
-
-// #8. Copyright 현재 년도
-const thisYear = document.querySelector(".this-year");
-thisYear.textContent = new Date().getFullYear();
